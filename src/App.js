934855ff39d95a762dd6d73af4144a1adc7e1c9f
kenethJS <123434225+kenethJS@users.noreply.github.com>
2023-01-24 16:56:39 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <h1>Keneth Evasco Javier</h1>
+    </div>
+  );
+}
+
+export default App;
